d3bd226bec53d4796863263574d0b2ef41601d8d
Oli Legg <oliglegg@hotmail.co.uk>
2018-11-27 18:39:09 +0000

    A lot of changes. Working well for now

--- typing-2/main.lua
+++ typing-2/main.lua
@@ -3,12 +3,13 @@ require("typing")
 function love.load()
     font = love.graphics.getFont()
     inputs = { 
-        typing:create(200,200,200,font:getHeight("W")+1),
-        --typing:create(200,220,200,font:getHeight("W")+1)
+        typing:create(200, 200, 200, font:getHeight("W")+1),
+        typing:create(200, 220, 250, (font:getHeight("W")+1)*2)
     }
 end
 
 function love.update(dt)
+    mouse()
     for i = 1, #inputs do
         inputs[i]:update(dt)
     end
@@ -18,23 +19,12 @@ function love.draw()
     for i = 1, #inputs do
         inputs[i]:draw()
     end
+    mouse_reset()
 end
 
 function love.textinput(str)
     for i = 1, #inputs do
         inputs[i]:textinput(str)
-    end
-end
-
-function love.mousepressed()
-    for i = 1, #inputs do
-        inputs[i]:mouseDown()
-    end
-end
-
-function love.mousereleased()
-    for i = 1, #inputs do
-        inputs[i]:mouseUp()
     end
 end
 
--- typing-2/typing.lua
+++ typing-2/typing.lua
@@ -1,6 +1,9 @@
 local utf8 = require("utf8")
 
-local pressed = false
+local released = false
+local pressing = false
+local press    = false
+
 local FONT = love.graphics.getFont()
 local focused_input = nil
 
@@ -18,9 +21,10 @@ local selected_text_i = cursor_letter_index
 local selected_text_w = 0
 local key_shifting = false -- If user is holding down shift
 local previous_click_time = 0
-local doubleclicktime = 0.2
+local DOUBLECLICKTIME = 0.2
 local doubleclicked = false
 local txtbox_mbDown = false -- If the first mb1 was inside of the box (used for dragging text)
+local id = 1
 love.keyboard.setKeyRepeat(true) -- enable key repeat so backspace can be held down to trigger love.keypressed multiple times.
 
 typing = {}
@@ -32,15 +36,42 @@ function typing:create(x, y, w, h)
 		w = w,
 		h = h,
 		text = "Insert text here",
-		displaying_text = "",
 		focus = false,
+		id = id
 	}, self)
+	id = id + 1
 	table.insert( typing, metatable )
 	return metatable
 end
 
+function typing:update(dt)
+	if self:selected() then
+		self.focus = true
+		self:mouseDown()
+		focused_input = getTypingIndex(self)
+		txtbox_mbDown = true
+	end
+	if self.focus then
+		if press then
+			cursor()
+		end
+		unfocus()
+		self.focus = true
+		focused_input = getTypingIndex(self)
+		cursor_blink()
+		if love.mouse.isDown(1) and txtbox_mbDown then
+			if not doubleclicked then
+				cursor()
+			end
+			selected_text_w = getSelectionWidth(selected_text_i, cursor_letter_index )
+		elseif love.mouse.isDown(1) and not txtbox_mbDown then
+			selected_text_w = 0
+			selected_text_i = cursor_letter_index
+		end
+	end
+end
+
 function typing:draw()
-	local display_text = self:scroll_along_limiter(self.text)
 	-- Box drawing
 	love.graphics.setColor(0.25,0.25,0.25,1)
 	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
@@ -67,32 +98,19 @@ function typing:draw()
 	
 	love.graphics.setColor(1,1,1,1)
 	-- Prints the text
-	love.graphics.printf(self.text, self.x+1, self.y, love.graphics.getWidth())
+	love.graphics.printf(self.text, self.x+1, self.y, self.x+self.w)
 
 end
 
 function typing:textinput(str)
-	if selection_isSelected() then
-		selection_delete()
-	end
-	self.text = string_insert(self.text, cursor_letter_index, str)
-	cursor_letter_index = cursor_letter_index + 1
-	selected_text_i = cursor_letter_index
-end
-
-function typing:update(dt)
 	if self.focus then
-		cursor_y = self.y
-		if love.mouse.isDown(1) and txtbox_mbDown then
-			if not doubleclicked then
-				cursor()
-			end
-			selected_text_w = getSelectionWidth(selected_text_i, cursor_letter_index )
-		elseif love.mouse.isDown(1) and not txtbox_mbDown then
-			selected_text_w = 0
-			selected_text_i = cursor_letter_index
+		if selection_isSelected() then
+			selection_delete()
 		end
-		cursor_blink()
+		local new_text = self:boxTypingLimit(str..self.text)
+		self.text = self:boxTypingLimit(string_insert(self.text, cursor_letter_index, str))
+		cursor_letter_index = cursor_letter_index + self.text:len()+1 - new_text:len()
+		selected_text_i = cursor_letter_index
 	end
 end
 
@@ -119,14 +137,16 @@ function typing:keyPressed(key)
 			cursor_deleteJump()
 		elseif key == "delete" then
 			cursor_delete()
+		elseif key == "return" then
+			self.focus = false
 		elseif key == "f1" then
 			print(cursor_letter_index, selected_text_i)
-		elseif love.keyboard.isDown('lctrl') and key == "a" then
+		elseif love.keyboard.isDown('lctrl') and key == "a" then -- selects all
 			selection_all()
-		elseif love.keyboard.isDown('lctrl') and key == "c" then
+		elseif love.keyboard.isDown('lctrl') and key == "c" then -- copy
 			selection_copy()
-		elseif love.keyboard.isDown('lctrl') and key == "v" then
-			text_insert(love.system.getClipboardText())
+		elseif love.keyboard.isDown('lctrl') and key == "v" then -- paste
+			self:text_insert(love.system.getClipboardText())
 		end
 		cursor_reset()
 		selection_update()
@@ -142,12 +162,70 @@ function typing:keyReleased(key)
 	end
 end
 
--- The text to be displayed in the input box
+function typing:setInput(str)
+	self.text = self:boxTypingLimit(str)
+end
+
+function typing:getInput(str)
+	return self.text
+end
+
+function typing:focus(str)
+	return self.focus
+end
+
+-- The text to be displayed in the input box 
 function typing:scroll_along_limiter(t)
 	if ((self.x + 1) + FONT:getWidth(t)) >= ((self.x + 1) + self.w) then
 		return self:scroll_along_limiter(t:sub(2, t:len()))
 	else
 		return t
+	end
+end
+
+function typing:mouseDown()
+	released = true
+	txtbox_mbDown = self:selected()
+	self.focus = txtbox_mbDown
+	if self.focus then
+		cursor()
+	end
+	selected_text_x = self:getCursorX(cursor_letter_index)
+	selected_text_i = cursor_letter_index
+	if (love.timer.getTime() - DOUBLECLICKTIME < previous_click_time) and txtbox_mbDown and self.focus then
+		selection_all()
+		doubleclicked = true
+	end
+	previous_click_time = love.timer.getTime()
+end
+
+function typing:mouseUp()
+	released = false
+	if txtbox_mbDown then
+		txtbox_mbDown = false
+	end
+	if doubleclicked then
+		doubleclicked = false
+	end
+end
+
+-- Whether this text box is selected or not
+function typing:selected()
+	local mx = love.mouse.getX() 
+	local my = love.mouse.getY()
+	if mx > self.x and mx < self.x+self.w and my > self.y and my < self.y+self.h and press then
+		return true
+	else
+		return false
+	end
+end
+
+-- Returns true if it changed the text
+function typing:boxTypingLimit(str)
+	if FONT:getWidth(str) > self.w then
+		return self:boxTypingLimit(str:sub(0,str:len()-1))
+	else
+		return str
 	end
 end
 
@@ -183,38 +261,13 @@ function string_remove(str, index)
 	return p1..p2
 end
 
-function typing:mouseDown()
-	pressed = true
-	txtbox_mbDown = self:selected()
-	cursor()
-	selected_text_x = self:getCursorX(cursor_letter_index)
-	selected_text_i = cursor_letter_index
-	if (love.timer.getTime() - doubleclicktime < previous_click_time) and txtbox_mbDown then
-		self:selection_all()
-		doubleclicked = true
+function getTypingIndex(x)
+	for i = 1, #typing do
+		if typing[i] == x then
+			return i
+		end
 	end
-	previous_click_time = love.timer.getTime()
-end
-
-function typing:mouseUp()
-	pressed = false
-	if txtbox_mbDown then
-		txtbox_mbDown = false
-	end
-	if doubleclicked then
-		doubleclicked = false
-	end
-end
-
--- Whether this text box is selected or not
-function typing:selected()
-	local mx = love.mouse.getX() 
-	local my = love.mouse.getY()
-	if mx > self.x and mx < self.x+self.w and my > self.y and my < self.y+self.h and pressed then
-		return true
-	else
-		return false
-	end
+	return nil
 end
 
 -- Moves the cursor based on the click and makes sure it hits in the box
@@ -222,16 +275,17 @@ function cursor()
 	local mx = love.mouse.getX() 
 	local my = love.mouse.getY()
 	for t = 1, #typing do
-		for i = 0, typing[t].text:len() do
-			local prev_char = FONT:getWidth(typing[t].text:sub(i,i)) / 2
-			local next_char = FONT:getWidth(typing[t].text:sub(i+1,i+1)) / 2
-			if mx >= typing[t].x+FONT:getWidth(typing[t].text:sub(0,i))-prev_char and mx <= typing[t].x+FONT:getWidth(typing[t].text:sub(1,i))+next_char and (my >= typing[t].y and my <= typing[t].y+typing[t].h or txtbox_mbDown) then
-				cursor_letter_index = i
-				unfocus()
-				typing[t].focus = true
-				focused_input = t
-				cursor_reset()
-				return
+		if typing[t].focus then
+			for i = 0, typing[t].text:len() do
+				local prev_char = FONT:getWidth(typing[t].text:sub(i,i)) / 2
+				local next_char = FONT:getWidth(typing[t].text:sub(i+1,i+1)) / 2
+				if mx >= typing[t].x+FONT:getWidth(typing[t].text:sub(0,i))-prev_char and mx <= typing[t].x+FONT:getWidth(typing[t].text:sub(1,i))+next_char and (my >= typing[t].y and my <= typing[t].y+typing[t].h or txtbox_mbDown) then
+					cursor_letter_index = i
+					--print(typing[t].id)
+					cursor_y = typing[t].y
+					cursor_reset()
+					return
+				end
 			end
 		end
 	end
@@ -359,9 +413,9 @@ end
 
 function selection_copy()
 	if cursor_letter_index > selected_text_i then
-		love.system.setClipboardText( text:sub(selected_text_i+1,cursor_letter_index))
+		love.system.setClipboardText( typing[focused_input].text:sub(selected_text_i+1,cursor_letter_index))
 	else
-		love.system.setClipboardText( text:sub(cursor_letter_index+1, selected_text_i))
+		love.system.setClipboardText( typing[focused_input].text:sub(cursor_letter_index+1, selected_text_i))
 	end
 end
 
@@ -404,7 +458,7 @@ function selection_text_selected()
 end 
 
 function typing:text_insert(str)
-	self.text = self.text:sub(1, math.min(cursor_letter_index, selected_text_i))..str..self.text:sub(math.max(cursor_letter_index+1, selected_text_i+1), self.text:len()+1)
+	--self:setInput(self.text:sub(1, math.min(cursor_letter_index, selected_text_i))..str..self.text:sub(math.max(cursor_letter_index+1, selected_text_i+1), self.text:len()+1))
 	cursor_letter_index = math.min(cursor_letter_index, selected_text_i)+str:len()
 	selected_text_i = cursor_letter_index
 	selected_text_w = 0
@@ -415,4 +469,30 @@ function unfocus()
 		typing[i].focus = false
 	end
 	focused_input = nil
+end
+
+function mouse()
+	if love.mouse.isDown(1) and not pressing then 
+		press = true
+		released = false
+	end
+	if not love.mouse.isDown(1) and pressing then
+		press = false
+		released = true
+	end
+	if love.mouse.isDown(1) then
+		pressing = true
+	else
+		pressing = false
+	end
+end
+
+function mouse_reset()
+	if released or press then 
+		if press then
+			press = false
+		elseif released then
+			released = false
+		end
+	end
 end
