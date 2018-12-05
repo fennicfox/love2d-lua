aafb2d8e32f889c7b239116650bc07796c9e2e28
Oli Legg <oliglegg@hotmail.co.uk>
2018-12-05 18:24:03 +0000

    Handling white space

--- practice_game/main.lua
+++ practice_game/main.lua
@@ -18,9 +18,9 @@ require 'discord.discord'
 
 function love.load()
 	-- Default testing font is useful for displaying information when debugging
-	graphFont 		 = love.graphics.newFont('font/SourceSansPro-Bold.ttf', 12)
-	click_sfx		 = love.audio.newSource('sfx/click.ogg',"static")
-	gamestate        = "Main menu"				   
+	graphFont        = love.graphics.newFont('font/SourceSansPro-Bold.ttf', 12)
+	click_sfx        = love.audio.newSource('sfx/click.ogg',"static")
+	gamestate        = "Main menu"
 	selected_level   = "practice_game/default.oli"-- I should replace .oli with .json I didn't know .json existed till recently
 	
 	-- Loading starting methods
@@ -29,25 +29,25 @@ function love.load()
 end
 
 function love.mousereleased( x, y, button )
-	mpressed = true								 -- True for one step when they release the mouse button
-	pressedm = button							 -- The value that the player takes their finger off the mouse (lmb = 1 rmb = 2)
+	mpressed = true                              -- True for one step when they release the mouse button
+	pressedm = button                            -- The value that the player takes their finger off the mouse (lmb = 1 rmb = 2)
 	--print("x","y")
 	--print(x,y,"\n")
 end
 
 function love.keyreleased( key )
-	kpressed = true 							 -- True for one step when the player takes their finger off a button
-	pressedk = key 								 -- The value that the player takes their finger off 
+	kpressed = true                              -- True for one step when the player takes their finger off a button
+	pressedk = key                               -- The value that the player takes their finger off 
 end
 
 function love.keypressed( key )
-	kdown = true 								 -- True for one step when the player puts their finger on a button
-	kname = key  								 -- The value that the player takes their finger off
+	kdown = true                                 -- True for one step when the player puts their finger on a button
+	kname = key                                  -- The value that the player takes their finger off
 	if key == "escape" then click_sfx:play() end -- I will remove this annoying sound in the future
 end
 
 function love.wheelmoved( x, y )
-	if 		y > 0 then  mwheelup   = true
+	if      y > 0 then  mwheelup   = true
 	elseif  y < 0 then  mwheeldown = true
 	end
 end
@@ -60,10 +60,10 @@ function love.update(dt)
 	
 	-- Always displaying discord API
 	discord_update()
-	if      gamestate == "Main menu"    	 then  main_menu_update(dt)
-	elseif  gamestate == "Playing"      	 then  playing_update(dt)
-	elseif  gamestate == "Paused"       	 then  paused_update(dt)
-	elseif  gamestate == "Level editor" 	 then  level_editor_update(dt)
+	if      gamestate == "Main menu"         then  main_menu_update(dt)
+	elseif  gamestate == "Playing"           then  playing_update(dt)
+	elseif  gamestate == "Paused"            then  paused_update(dt)
+	elseif  gamestate == "Level editor"      then  level_editor_update(dt)
 	end
 end
 
@@ -73,10 +73,10 @@ function love.draw()
 	love.graphics.setFont(graphFont, 12)
 	love.graphics.setColor(1,1,1,1)
 	love.graphics.print(gamestate, love.graphics.getWidth()-80, love.graphics.getHeight()-20)
-	if      gamestate == "Main menu"  	 	 then  main_menu_draw()
-	elseif  gamestate == "Playing"    	 	 then  playing_draw()
-	elseif  gamestate == "Paused"     	 	 then  paused_draw() 
-	elseif  gamestate == "Level editor"  	 then  level_editor_draw() end
+	if      gamestate == "Main menu"         then  main_menu_draw()
+	elseif  gamestate == "Playing"           then  playing_draw()
+	elseif  gamestate == "Paused"            then  paused_draw() 
+	elseif  gamestate == "Level editor"      then  level_editor_draw() end
 	love.graphics.setFont(graphFont, 12) 
 	love.graphics.setColor(1,1,1,1)
 
@@ -90,8 +90,8 @@ end
 
 -- When the window resizes, it is important that the GUI and general graphics scale with it.
 function love.resize( w, h )
-	if 	    gamestate == "Main menu" 		 then main_menu_resize(w, h)
-	elseif  gamestate == "Paused"    		 then paused_resize(w, h)
+	if      gamestate == "Main menu"         then main_menu_resize(w, h)
+	elseif  gamestate == "Paused"            then paused_resize(w, h)
 	elseif  gamestate == "Level editor menu" then level_editor_menu_resize(w, h) 
 	end
 end
@@ -104,5 +104,8 @@ end
 
 -- I've made this function global because it's really useful in all parts of the game
 function round( n, p )
-	return math.floor(n/p)*p
+	local number = n % p
+	if number > p/2 then
+		return n + (n - p/2)
+	elseif number 
 end
